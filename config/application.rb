63cd398528bbe3d715501576420ef5df2b0e1303
henchiyb <ducnhanyb@gmail.com>
2018-07-10 10:14:37 +0700

    Init rubocop

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,13 @@
+<<<<<<< HEAD
 require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
+=======
+require_relative "boot"
+
+require "rails"
+>>>>>>> 87bdd57... Add rubocop
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -11,16 +17,19 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+<<<<<<< HEAD
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+=======
+>>>>>>> 87bdd57... Add rubocop
 Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+<<<<<<< HEAD
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -28,6 +37,8 @@ module SampleApp
     # the framework and any gems in your application.
 
     # Don't generate system test files.
+=======
+>>>>>>> 87bdd57... Add rubocop
     config.generators.system_tests = nil
   end
 end
--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+=======
+# frozen_string_literal: true
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+>>>>>>> 87bdd57... Add rubocop
+
+require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
