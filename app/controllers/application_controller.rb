cf4007be17337b889d243ad9d71d0473191ebbea
henchiyb <ducnhanyb@gmail.com>
2018-07-10 11:12:54 +0700

    Chapter 3: Static Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello World"
+  end
 end
--- config/boot.rb
+++ config/boot.rb
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-=======
-# frozen_string_literal: true
-
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
->>>>>>> 87bdd57... Add rubocop
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require "bundler/setup"
+require "bootsnap/setup"
