cea49eebc10ead49f0ab3a7011d21c7b98a99648
henchiyb <ducnhanyb@gmail.com>
2018-07-10 10:14:37 +0700

    Chapter 11: Account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,12 +2,12 @@ Rails.application.configure do
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -16,7 +16,19 @@ Rails.application.configure do
   end
 
   config.active_storage.service = :local
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  host = "0.0.0.0:3000"
+  config.action_mailer.default_url_options = { host: host, protocol: "http" }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "mail.google.com",
+    port: 587,
+    user_name: ENV["EMAIL_SEND"],
+    password: ENV["EMAIL_PASS"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
